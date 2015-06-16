42a8d7eb4c39b69293dde9ee0d2f4e57bf7d123b
Holman Rivera <holmat99@gmail.com>
2015-06-15 21:07:56 -0500

    first Checkin

--- web_viewer/dino_server.py
+++ web_viewer/dino_server.py
@@ -12,6 +12,10 @@ class DinoFilter(tornado.web.RequestHandler):
     def get(self):
         self.render("dino_filter.html")
 
+class DinoCheckin(tornado.web.RequestHandler):
+    def get(self):
+        self.render("stage_map.html")
+
 class FilterData(tornado.web.RequestHandler):
     def get(self):
         x_min = float(self.get_argument("x_min"))
@@ -31,7 +35,6 @@ class FilterData(tornado.web.RequestHandler):
     def initialize(self, df):
         self.df = df
 
-
 class DataHandler(tornado.web.RequestHandler):
     def get(self):
         df = self.df
@@ -48,20 +51,44 @@ class DataHandler(tornado.web.RequestHandler):
         self.df = df[["X","Y","id","Timestamp","type"]]
 
 
+class CheckinHandler(tornado.web.RequestHandler):
+    def get(self):
+        df= self.df        
+        data = df.loc[df["type"] == "check-in"]
+        data = data.groupby(['X', 'Y'])['X'].count()
+        data = pd.DataFrame(data)
+        data.columns = ['veces']
+        max_veces = data['veces'].max()
+        data['veces'] = abs((data['veces'] / max_veces) - 1)
+        #percent_value_include = 0.5
+        data.to_csv('C:/Users/Holman/Documents/visualAnalytics/MC1_2015_Data/data1.csv', index=True)   
+        data = pd.read_csv("C:/Users/Holman/Documents/visualAnalytics/MC1_2015_Data/data1.csv")
+        #percent_value_include_1 = data.sort('veces').get_value(np.int(np.ceil(data.shape[0]*percent_value_include)), 'veces')        
+        #data = data.loc[data['veces'] >= percent_value_include_1]
+        self.write({"array" :data.to_dict()})
+    
+    def initialize(self, df):
+        #self.df = df[["X","Y","veces"]]
+        self.df = df
+
+
 settings = {"template_path" : os.path.dirname(__file__),
             "static_path" : os.path.join(os.path.dirname(__file__),"static"),
             "debug" : True
             } 
 
 if __name__ == "__main__":
-    path = os.path.join(os.path.dirname(__file__), "C:/MC1 2015 Data/park-movement-Fri.csv")
+    path = os.path.join(os.path.dirname(__file__), "C:/Users/Holman/Documents/visualAnalytics/MC1_2015_Data/park-movement-Fri.csv")
     print('loading...')
     df = pd.read_csv(path)
     df["time"] = pd.to_datetime(df.Timestamp, format="%Y-%m-%d %H:%M:%S")
 
+    
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/data", DataHandler,{"df":df}),
+        (r"/data2", CheckinHandler,{"df":df}),
+        (r"/checkin", DinoCheckin),
         (r"/filter", DinoFilter),
         (r"/filter_data", FilterData,{"df":df}),
         (r"/static/(.*)", tornado.web.StaticFileHandler,
